17359af737ff5f54b0f83d83d1d73865ea694a95
Marc Davies <marcdaviesriot@gmail.com>
2020-04-01 21:11:51 -0700

    add addTransaction action

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,13 +1,29 @@
-import React, {useState} from "react";
+import React, {useState, useContext} from "react";
+import { v4 as uuidv4 } from 'uuid';
+import { GlobalContext } from "../context/GlobalState";
 
 export const AddTransaction = () => {
   const [text, setText] = useState('');
   const [amount, setAmount] = useState(0);
 
+  const { addTransaction } = useContext(GlobalContext);
+
+  const onSubmit = e => {
+    e.preventDefault();
+
+    const newTransaction = {
+      id: uuidv4(),
+      text,
+      amount: +amount
+    }
+
+    addTransaction(newTransaction);
+  }
+
   return (
     <>
       <h3>Add new transaction</h3>
-      <form id="form">
+      <form onSubmit={onSubmit}>
         <div className="form-control">
           <label htmlFor="text">Text</label>
           <input type="text" value={text} onChange={(e) => setText(e.target.value)} placeholder="Enter text..." />
