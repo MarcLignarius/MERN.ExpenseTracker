3383653f28a5f19fa9d5dc54f9b0e0c2f7730e5b
Marc Davies <marcdaviesriot@gmail.com>
2020-04-03 15:06:04 -0700

    use production build

---
+++ client/build/precache-manifest.e85f49c1cee95be0499be50d4f3c72d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5009fa69f7755adce7c28be1a6e35b11",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dab1e5392c046009782c",
+    "url": "/static/css/main.caf1db2f.chunk.css"
+  },
+  {
+    "revision": "1f56580576831ae6ff0b",
+    "url": "/static/js/2.457ab9d2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.457ab9d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dab1e5392c046009782c",
+    "url": "/static/js/main.0a87f0ed.chunk.js"
+  },
+  {
+    "revision": "742c19565e4dd7340926",
+    "url": "/static/js/runtime-main.0c8d5db1.js"
+  }
+]);
