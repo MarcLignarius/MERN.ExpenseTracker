63451623bcc53c2b0177189ac3f55bfee7cf4372
Marc Davies <marcdaviesriot@gmail.com>
2020-04-03 16:22:45 -0700

    remove mongo db uri

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect('mongodb+srv://marcdaviesriot:TDvmzS7TksdhUYKM@reactexpensetracker-rdp7q.mongodb.net/expensetracker?retryWrites=true&w=majority', {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useCreateIndex: true,
       useUnifiedTopology: true
