9f255c53f954d4147b622603c4117be46ef7c48e
Marc Davies <marcdaviesriot@gmail.com>
2020-04-03 15:01:05 -0700

    add missing dispatch

--- client/src/context/GlobalState.js
+++ client/src/context/GlobalState.js
@@ -36,6 +36,11 @@ export const GlobalProvider = ({ children }) => {
   async function deleteTransaction(id) {
     try {
       await axios.delete(`/api/v1/transactions/${id}`);
+
+      dispatch({
+        type: 'DELETE_TRANSACTION',
+        payload: id
+      });
     } catch (err) {
       dispatch({
         type: "TRANSACTION_ERROR",
